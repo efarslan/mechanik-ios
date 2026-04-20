819634d81084c76de12121005c8e2744574e37c8
Efe <efearslannn11@gmail.com>
2026-04-20 23:13:15 +0300

    Add initial Mechanik SwiftUI app scaffold

---
+++ Mechanik/Mechanik/MechanikApp.swift
@@ -0,0 +1,17 @@
+//
+//  MechanikApp.swift
+//  Mechanik
+//
+//  Created by efe arslan on 20.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct MechanikApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Mechanik/MechanikTests/MechanikTests.swift
@@ -0,0 +1,19 @@
+//
+//  MechanikTests.swift
+//  MechanikTests
+//
+//  Created by efe arslan on 20.04.2026.
+//
+
+import Testing
+@testable import Mechanik
+
+struct MechanikTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
